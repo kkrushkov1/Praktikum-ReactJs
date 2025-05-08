02166f4f9e1a04741d2d0dd04cec1dec54e1431b
Cocky <123155811+kkrushkov1@users.noreply.github.com>
2025-05-08 17:30:40 +0300

    Make create game form controlled

--- client/src/components/CreateGame/CreateGame.js
+++ client/src/components/CreateGame/CreateGame.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { GameContext } from "../../contexts/GameContext";
 import * as gameService from "../../services/gameService";
@@ -7,20 +7,49 @@ export const CreateGame = ({}) => {
     const { gameAdd } = useContext(GameContext);
     const navigate = useNavigate();
 
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [maxLevel, setMaxLevel] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [summary, setSummary] = useState("");
+
     const onSubmit = (e) => {
         e.preventDefault();
 
-        const gameData = Object.fromEntries(new FormData(e.target));
-
+        const gameData = {
+            title,
+            category,
+            maxLevel,
+            imageUrl,
+            summary,
+        };
         gameService.create(gameData).then((result) => {
             gameAdd(result);
             navigate("/catalog");
         });
     };
 
+    const changeTitleHandler = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const changeCategoryHandler = (e) => {
+        setCategory(e.target.value);
+    };
+
+    const changeMaxLevelHandler = (e) => {
+        setMaxLevel(e.target.value);
+    };
+
+    const changeImageUrlHandler = (e) => {
+        setImageUrl(e.target.value);
+    };
+    const changeSummaryHandler = (e) => {
+        setSummary(e.target.value);
+    };
+
     return (
         <section id="create-page" className="auth">
-            {/* Uncontrolled forms */}
             <form id="create" onSubmit={onSubmit}>
                 <div className="container">
                     <h1>Create Game</h1>
@@ -30,6 +59,8 @@ export const CreateGame = ({}) => {
                         id="title"
                         name="title"
                         placeholder="Enter game title..."
+                        value={title}
+                        onChange={changeTitleHandler}
                     />
                     <label htmlFor="category">Category:</label>
                     <input
@@ -37,6 +68,8 @@ export const CreateGame = ({}) => {
                         id="category"
                         name="category"
                         placeholder="Enter game category..."
+                        value={category}
+                        onChange={changeCategoryHandler}
                     />
                     <label htmlFor="levels">MaxLevel:</label>
                     <input
@@ -45,6 +78,8 @@ export const CreateGame = ({}) => {
                         name="maxLevel"
                         min={1}
                         placeholder={1}
+                        value={maxLevel}
+                        onChange={changeMaxLevelHandler}
                     />
                     <label htmlFor="game-img">Image:</label>
                     <input
@@ -52,9 +87,16 @@ export const CreateGame = ({}) => {
                         id="imageUrl"
                         name="imageUrl"
                         placeholder="Upload a photo..."
+                        value={imageUrl}
+                        onChange={changeImageUrlHandler}
                     />
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary" defaultValue={""} />
+                    <textarea
+                        name="summary"
+                        id="summary"
+                        value={summary}
+                        onChange={changeSummaryHandler}
+                    />
                     <input
                         className="btn submit"
                         type="submit"
